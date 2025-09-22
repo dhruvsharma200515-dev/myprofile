15c76c623ffd214cf54732e351d41b192d1e4bb0
Aditya <aditya2005ads@gmail.com>
2025-09-22 21:37:43 +0530

    final try

--- profile/vite.config.js
+++ profile/vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'copy-redirects',
-          closeBundle() {
-            const srcPath = path.resolve('_redirects')       // root file
-            const distPath = path.resolve('dist/_redirects') // copy into dist
-            if (fs.existsSync(srcPath)) {
-              fs.copyFileSync(srcPath, distPath)
-              console.log('_redirects copied to dist/')
-            }
-          }
-        }
-      ]
-    }
-  }
 })
